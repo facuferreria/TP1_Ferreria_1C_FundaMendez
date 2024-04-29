1e7b3c4ec2dfcfa029111e355e7e88e7510ad903
tobiasWaizer <twaizer@frba.utn.edu.ar>
2024-04-29 17:53:28 +0000

    lo siguiente a hacer es validar que no se superpongan los elementos

--- feria.c
+++ feria.c
@@ -1,5 +1,123 @@
-#include <feria.h>
+#include <stdio.h>
+#include <stdlib.h> // Para usar rand
+#include <time.h> // Para obtener una semilla desde el reloj
+#include "feria.h"
+
+#define FILAS 20
+#define COLUMNAS 20
+
+coordenada_t asignar_posicion(){
+    coordenada_t posicion;
+    posicion.col = rand() % 20;
+    posicion.fil = rand() % 20;
+    return posicion;
+}
+
+void inicializar_personaje(personaje_t* personaje){
+    srand((unsigned)time(NULL));
+
+    personaje->vida = 3;
+    personaje->energia = 100;
+    personaje->camuflado = false;
+    personaje->posicion = asignar_posicion();
+    printf("Vida del personaje: %d\n", personaje->vida);
+    printf("Posicion inicial personaje: %d, %d\n", personaje->posicion.fil, personaje->posicion.col);
+}
+
+void inicializar_bombas(bomba_t* bombas, int tope_bombas){
+    srand((unsigned)time(NULL));
+
+    tope_bombas = 10;
+
+    for (int i = 0; i < tope_bombas; i++){
+
+        bombas->posicion = asignar_posicion();
+        bombas->timer = rand() % 250 + 50;
+        bombas->desactivada = false;
+
+        printf("Posicion inicial bomba %d: %d, %d con respectivo timer de %d\n", i, bombas->posicion.fil, bombas->posicion.col, bombas->timer);
+    }    
+}
+
+void inicializar_herramientas(herramienta_t* herramientas, int tope_herramientas){
+    srand((unsigned)time(NULL));
+
+    tope_herramientas = 8;
+
+    for (int i = 0; i < tope_herramientas; i++){
+
+        herramientas->posicion = asignar_posicion();
+        
+        if (i < 5){ 
+            herramientas->tipo = 'G';
+        } 
+        
+        else {
+            herramientas->tipo = 'S';
+        }
+        
+        printf("Posicion inicial herramientas %d: %d, %d de tipo %c\n", i, herramientas->posicion.fil, herramientas->posicion.col, herramientas->tipo);
+    }    
+}
+
+
+void inicializar_familiares(familiar_t * familiares, int tope_familiares){
+    srand((unsigned)time(NULL));
+
+    tope_familiares = 3;
+
+    for (int i = 0; i < tope_familiares; i++){
+
+        familiares->posicion = asignar_posicion();
+        
+        if (i ==  0){ 
+            familiares->inicial_nombre = 'H';
+        } 
+
+        else if (i == 1)
+        {
+            familiares->inicial_nombre = 'F';
+        }
+        
+        
+        else {
+            familiares->inicial_nombre = 'C';
+        }
+        
+        printf("Posicion inicial familiar %d: %d, %d de familiar %c\n", i, familiares->posicion.fil, familiares->posicion.col, familiares->inicial_nombre);
+    }    
+}
 
 void inicializar_juego(juego_t* juego){
-    //CODIGO QUE CUMPLA CON LAS PRE Y POST CONDICIONES
+    
+    inicializar_personaje(&(juego->perry));
+    inicializar_bombas((juego->bombas), (juego->tope_bombas));
+    inicializar_herramientas((juego->herramientas), (juego->tope_herramientas));
+    inicializar_familiares((juego->familiares), (juego->tope_familiares));
+}
+
+
+void imprimir_terreno(juego_t juego){
+     char matriz[FILAS][COLUMNAS];
+
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            matriz[i][j] = '.';
+        }
+    }
+
+    for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            printf("%c ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void validar_posicion(posicion){
+        /*for (int i = 0; i < FILAS; i++) {
+        for (int j = 0; j < COLUMNAS; j++) {
+            matriz[i][j] = '.';
+        }
+    }*/
 }
--- juego.c
+++ juego.c
@@ -1,33 +1,30 @@
-#include <stdio.h>
-#include <feria.c>
+#include "feria.c"
 
 int main(){
-    char respuesta_jugador[];
+    char respuesta_jugador;
 
-    printf("Bienvenido jugador! Su mision es ayudar al Agente P que debe salvar a la feria de la ciudad desactivando las bombas del malvado Dr. Doofenshmirtz.\n Pero tengan cuidado, 
-        la familia del Agente P esta en la feria tambien, por ende debe hacerlo siempre y cuando su identidad se mantenga oculta. \n
-        Buena suerte Agentes... ");
+    printf("Bienvenido jugador! Su mision es ayudar al Agente P que debe salvar a la feria de la ciudad desactivando las bombas del malvado Dr. Doofenshmirtz.\n Pero tengan cuidado,  la familia del Agente P esta en la feria tambien, por ende debe hacerlo siempre y cuando su identidad se mantenga oculta. \n Buena suerte Agentes... ");
 
     //ACA va la inicializacion del mapa, inicializacion de las bombas, objetos, familia y perry ya que solo va a pasar una vez
 
-    printf("Por favor elija algunas de las siguientes teclas para mover al Agente P o camuflarlo: \n
-        - W para moverse ARRIBA \n
-        - A para moverse IZQUIERDA \n
-        - S para moverse ABAJO \n
-        - D para moverse DERECHA \n
-        - Q para moverse CAMUFLARSE \n");
-    scanf("%s", &respuesta_jugador);
+    printf("Por favor elija algunas de las siguientes teclas para mover al Agente P o camuflarlo: \n - W para moverse ARRIBA \n - A para moverse IZQUIERDA \n - S para moverse ABAJO \n - D para moverse DERECHA \n- Q para moverse CAMUFLARSE \n");
 
-    printf("¿Queres salvar el mundo con el Agente P? Responde SI o NO si queres jugar \n");
-    scanf("%s", &respuesta_jugador);
+    printf("¿Queres salvar el mundo con el Agente P? Responde S para SI o N para NO si queres jugar \n");
+    scanf(" %c", &respuesta_jugador);
 
-    while (respuesta_jugador != "NO"){
+    juego_t juego;
+
+    inicializar_juego(&juego);
+
+    imprimir_terreno(juego);
+
+    while (respuesta_jugador != 'N'){
         
         //ACA van los movimientos del usuario con perry ya sea para moverse o cambiar de camuflaje
 
-
-        printf("Por favor elija otra tecla para ejecutar una accion o escriba NO para salir del juego");
-        scanf("%s", &respuesta_jugador);
+        scanf("%c", &respuesta_jugador);
+        printf("Por favor elija otra tecla para ejecutar una accion o escriba NO para salir del juego\n");
+        
     }
 
     printf("Termino el juego...");
