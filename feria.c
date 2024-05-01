75e9d9e934cd59aee768942b924925865f01bb3f
tobiasWaizer <twaizer@frba.utn.edu.ar>
2024-05-01 19:53:27 +0000

    Funcionamiento completo de inicializacion y grafico de juego

--- feria.c
+++ feria.c
@@ -13,6 +13,18 @@ coordenada_t asignar_posicion(){
     return posicion;
 }
 
+void verificar_camuflaje(bool esta_camuflado){
+    if (esta_camuflado)
+    {
+        printf("\nCamuflaje: Activado\n");
+    }
+    else
+    {
+        printf("\nCamuflaje: Desactivado\n");
+    }
+    
+}
+
 void inicializar_personaje(personaje_t* personaje){
     srand((unsigned)time(NULL));
 
@@ -20,104 +32,183 @@ void inicializar_personaje(personaje_t* personaje){
     personaje->energia = 100;
     personaje->camuflado = false;
     personaje->posicion = asignar_posicion();
-    printf("Vida del personaje: %d\n", personaje->vida);
-    printf("Posicion inicial personaje: %d, %d\n", personaje->posicion.fil, personaje->posicion.col);
 }
 
-void inicializar_bombas(bomba_t* bombas, int tope_bombas){
+void inicializar_bombas(bomba_t* bombas, personaje_t* personaje, juego_t* juego){
     srand((unsigned)time(NULL));
 
-    tope_bombas = 10;
+    juego->tope_bombas = 10;
 
-    for (int i = 0; i < tope_bombas; i++){
+    for (int i = 0; i < juego->tope_bombas; i++){
 
-        bombas->posicion = asignar_posicion();
-        bombas->timer = rand() % 250 + 50;
-        bombas->desactivada = false;
-
-        printf("Posicion inicial bomba %d: %d, %d con respectivo timer de %d\n", i, bombas->posicion.fil, bombas->posicion.col, bombas->timer);
+        do
+        {
+            juego->bombas[i].posicion = asignar_posicion();
+        } while (bombas->posicion.fil == personaje->posicion.fil && bombas->posicion.col == personaje->posicion.col);
+        
+        juego->bombas[i].timer = rand() % 250 + 50;
+        juego->bombas[i].desactivada = false;
     }    
 }
 
-void inicializar_herramientas(herramienta_t* herramientas, int tope_herramientas){
+void inicializar_herramientas(herramienta_t* herramientas, bomba_t* bombas, personaje_t* personaje, juego_t* juego){
     srand((unsigned)time(NULL));
 
-    tope_herramientas = 8;
+    juego->tope_herramientas = 8;
 
-    for (int i = 0; i < tope_herramientas; i++){
+    for (int i = 0; i < juego->tope_herramientas; i++){
 
-        herramientas->posicion = asignar_posicion();
+        do
+        {
+            juego->herramientas[i].posicion = asignar_posicion();
+        } while (herramientas->posicion.fil == bombas->posicion.fil && herramientas->posicion.col == bombas->posicion.col || herramientas->posicion.fil == personaje->posicion.fil && herramientas->posicion.col == personaje->posicion.col);
         
         if (i < 5){ 
-            herramientas->tipo = 'G';
+            juego->herramientas[i].tipo = 'G';
         } 
         
         else {
-            herramientas->tipo = 'S';
+            juego->herramientas[i].tipo = 'S';
         }
-        
-        printf("Posicion inicial herramientas %d: %d, %d de tipo %c\n", i, herramientas->posicion.fil, herramientas->posicion.col, herramientas->tipo);
     }    
 }
 
 
-void inicializar_familiares(familiar_t * familiares, int tope_familiares){
+void inicializar_familiares(familiar_t * familiares, herramienta_t* herramientas, bomba_t* bombas, personaje_t* personaje, juego_t* juego){
     srand((unsigned)time(NULL));
 
-    tope_familiares = 3;
+    juego->tope_familiares = 3;
 
-    for (int i = 0; i < tope_familiares; i++){
+    for (int i = 0; i < juego->tope_familiares; i++){
 
-        familiares->posicion = asignar_posicion();
+         do
+        {
+            juego->familiares[i].posicion = asignar_posicion();
+        } while (familiares->posicion.fil == bombas->posicion.fil && familiares->posicion.col == bombas->posicion.col || familiares->posicion.fil == personaje->posicion.fil && familiares->posicion.col == personaje->posicion.col || familiares->posicion.fil == herramientas->posicion.fil && familiares->posicion.col == herramientas->posicion.col);
         
         if (i ==  0){ 
-            familiares->inicial_nombre = 'H';
+            juego->familiares[i].inicial_nombre = 'H';
         } 
 
         else if (i == 1)
         {
-            familiares->inicial_nombre = 'F';
+            juego->familiares[i].inicial_nombre = 'F';
         }
         
         
         else {
-            familiares->inicial_nombre = 'C';
+            juego->familiares[i].inicial_nombre = 'C';
         }
-        
-        printf("Posicion inicial familiar %d: %d, %d de familiar %c\n", i, familiares->posicion.fil, familiares->posicion.col, familiares->inicial_nombre);
     }    
 }
 
 void inicializar_juego(juego_t* juego){
-    
-    inicializar_personaje(&(juego->perry));
-    inicializar_bombas((juego->bombas), (juego->tope_bombas));
-    inicializar_herramientas((juego->herramientas), (juego->tope_herramientas));
-    inicializar_familiares((juego->familiares), (juego->tope_familiares));
+
+    inicializar_personaje(&juego->perry);
+    inicializar_bombas(juego->bombas, &(juego->perry), juego);
+    inicializar_herramientas(&(juego->herramientas), &(juego->bombas), &(juego->perry), juego);
+    inicializar_familiares(&(juego->familiares), &(juego->herramientas), &(juego->bombas), &(juego->perry), juego);
+
+    imprimir_terreno(*juego);
 }
 
 
 void imprimir_terreno(juego_t juego){
-     char matriz[FILAS][COLUMNAS];
 
-    for (int i = 0; i < FILAS; i++) {
+    char terreno[FILAS][COLUMNAS];
+
+    for(int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
-            matriz[i][j] = '.';
+            terreno[i][j] = '.';
         }
     }
+    
+   
+    //Asignamos primero posicion en terreno a perry
+    terreno[juego.perry.posicion.fil][juego.perry.posicion.col] = 'P';
 
+
+    //Asignamos las bombas
+    for (int i = 0; i < juego.tope_bombas; i++){
+        terreno[juego.bombas[i].posicion.fil][juego.bombas[i].posicion.col]= 'B'; 
+    }
+
+    //Asignamos herramientas
+    for (int i = 0; i < juego.tope_herramientas; i++){
+        terreno[juego.herramientas[i].posicion.fil][juego.herramientas[i].posicion.col] = juego.herramientas->tipo;
+    }
+
+    //Asignamos familiares
+    for (int i = 0; i < juego.tope_familiares; i++){
+        terreno[juego.familiares[i].posicion.fil][juego.familiares[i].posicion.col] = juego.familiares->inicial_nombre;
+    }
+
+    //imprimo mi matriz con los elementos en el terreno ya sin repetir
     for (int i = 0; i < FILAS; i++) {
         for (int j = 0; j < COLUMNAS; j++) {
-            printf("%c ", matriz[i][j]);
+            printf("%c ", terreno[i][j]);
         }
         printf("\n");
     }
+
+    //Imprimo datos de Perry
+    printf("\nVidas de Perry: %d\n", juego.perry.vida);
+    printf("\nVidas de Perry: %d\n", juego.perry.energia);
+    verificar_camuflaje(juego.perry.camuflado);
 }
 
-void validar_posicion(posicion){
-        /*for (int i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
-            matriz[i][j] = '.';
+
+
+
+
+
+void camuflar_personaje(juego_t juego){
+    if (juego.perry.camuflado == false){
+        juego.perry.camuflado == true;
+    }
+    else{
+        juego.perry.camuflado == false;
+    }
+    
+}
+
+void realizar_jugada(juego_t* juego, char accion){
+    printf("\nFila del agente: %d -- Col del agente: %d\n", juego->perry.posicion.fil, juego->perry.posicion.col);
+    if (juego->perry.posicion.fil <= 20 && juego->perry.posicion.col <= 20){
+        switch (accion){
+            case 'W':
+                juego->perry.posicion.fil -= 1;
+                imprimir_terreno(*juego);
+                break;
+
+            case 'A':
+                juego->perry.posicion.col -= 1;
+                imprimir_terreno(*juego);
+                break;
+
+            case 'S':
+                juego->perry.posicion.fil += 1;
+                imprimir_terreno(*juego);
+                break;
+
+            case 'D':
+                juego->perry.posicion.col += 1;
+                imprimir_terreno(*juego);
+                break;
+
+            case 'Q':
+                camuflar_personaje(*juego);
+                imprimir_terreno(*juego);
+                break;
+            
+            default:
+                printf("\nMovimiento NO EXISTENTE! Elegi otro movimiento para el Agente P\n");
+                imprimir_terreno(*juego);
+                break;
         }
-    }*/
+    }
+    else{
+        printf("\nNo podes salirte del mapa! Elegi otro movimiento!\n");
+        imprimir_terreno(*juego);
+    }
 }
--- juego.c
+++ juego.c
@@ -1,29 +1,37 @@
 #include "feria.c"
 
 int main(){
+
     char respuesta_jugador;
 
-    printf("Bienvenido jugador! Su mision es ayudar al Agente P que debe salvar a la feria de la ciudad desactivando las bombas del malvado Dr. Doofenshmirtz.\n Pero tengan cuidado,  la familia del Agente P esta en la feria tambien, por ende debe hacerlo siempre y cuando su identidad se mantenga oculta. \n Buena suerte Agentes... ");
+    printf("\nBienvenido jugador! Su mision es ayudar al Agente P que debe salvar a la feria de la ciudad desactivando las bombas del malvado Dr. Doofenshmirtz.\n Pero tengan cuidado,  la familia del Agente P esta en la feria tambien, por ende debe hacerlo siempre y cuando su identidad se mantenga oculta. \n Buena suerte Agentes... ");
 
     //ACA va la inicializacion del mapa, inicializacion de las bombas, objetos, familia y perry ya que solo va a pasar una vez
 
-    printf("Por favor elija algunas de las siguientes teclas para mover al Agente P o camuflarlo: \n - W para moverse ARRIBA \n - A para moverse IZQUIERDA \n - S para moverse ABAJO \n - D para moverse DERECHA \n- Q para moverse CAMUFLARSE \n");
+    printf("\nPor favor elija algunas de las siguientes teclas para mover al Agente P o camuflarlo: \n - W para moverse ARRIBA \n - A para moverse IZQUIERDA \n - S para moverse ABAJO \n - D para moverse DERECHA \n- Q para moverse CAMUFLARSE \n");
 
-    printf("¿Queres salvar el mundo con el Agente P? Responde S para SI o N para NO si queres jugar \n");
+    printf("\n¿Queres salvar el mundo con el Agente P? Responde S para SI o N para NO si queres jugar \n");
     scanf(" %c", &respuesta_jugador);
 
     juego_t juego;
 
+    
+
     inicializar_juego(&juego);
 
-    imprimir_terreno(juego);
+
+    printf("\nPor favor elija una tecla para ejecutar una accion del Agente P\n");
+    scanf("%c", &respuesta_jugador);
 
     while (respuesta_jugador != 'N'){
         
         //ACA van los movimientos del usuario con perry ya sea para moverse o cambiar de camuflaje
 
+        realizar_jugada(&juego, respuesta_jugador);
+
+        printf("\nPor favor elija otra tecla para ejecutar una accion o escriba NO para salir del juego\n");
         scanf("%c", &respuesta_jugador);
-        printf("Por favor elija otra tecla para ejecutar una accion o escriba NO para salir del juego\n");
+
         
     }
 
